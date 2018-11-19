298e3c4cb5d11093cd5ee51aac7ce3b9a80dbe24
Marcos Junior <marcos.junio@ccc.ufcg.edu.br>
2018-11-19 14:10:31 -0300

    adição do time

--- bash.sh
+++ bash.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #gnome-terminal
 computer=$(cat /etc/hostname)
-caminho=$(pwd | sed "s/\/home\/$USER/~/" )
 
 while true 
 do
 
+caminho=$(pwd | sed "s/\/home\/$USER/~/" )
 echo -n $USER@$computer:$caminho"$ "; read comando
-echo $($comando)
+echo $comando
+#/usr/bin/time -f "$comando %e, %U, %S" -ao comandos $comando 
+$comando
 echo "Meu terminal" 
 done 
+
