08f23d0c5cefaf0115f621eb3b20796792a55db0
Marcos Júnior <marcos.junio@ccc.ufcg.edu.br>
2018-11-19 15:36:24 -0300

    funcionamento do | > <

--- bash.sh
+++ bash.sh
@@ -8,8 +8,15 @@ do
 caminho=$(pwd | sed "s/\/home\/$USER/~/" )
 echo -n $USER@$computer:$caminho"$ "; read comando
 echo $comando
-#/usr/bin/time -f "$comando %e, %U, %S" -ao comandos $comando 
-$comando
+help | grep -qw $comando
+
+if [ $? -eq 0 ]; then
+	/usr/bin/time -f "$comando %e, %U, %S" -ao comandos echo $comando | bash
+else
+	echo $comando | bash
+fi
+ 
 echo "Meu terminal" 
 done 
 
+cd /home/fulando grep
