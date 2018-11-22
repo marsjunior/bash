565c3862ce43cb116bf2887556f9bb2605da07ae
Marcos Junior <marcos.junio@ccc.ufcg.edu.br>
2018-11-22 00:03:40 -0300

    log

--- bash.sh
+++ bash.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
-#gnome-terminal
 computer=$(cat /etc/hostname)
-
-while true 
-do
+home=$(echo "/home/$USER")
+comando_shell=-1
+while true;do
 
 caminho=$(pwd | sed "s/\/home\/$USER/~/" )
 echo -n $USER@$computer:$caminho"$ "; read comando
-echo $comando
-help | grep -qw $comando
 
-if [ $? -eq 0 ]; then
-	/usr/bin/time -f "$comando %e, %U, %S" -ao comandos echo $comando | bash
-else
-	echo $comando | bash
+vazio=$(echo $comando | awk '{print $1}')
+argumentos=$(echo $comando | sed -e 's/ /\n/g')
+
+if [ -n "$vazio" ]; then
+
+	for x in $argumentos; do
+	x=$(echo $x | sed -e 's/-//g')
+	x=$(echo $x | sed -e 's/|//g')
+	tamanho=${#x}
+	if [ $tamanho -ge 2 ];then
+		help | grep -qw $x
+		comando_shell=$?
+		if [ "$x" = 'cd' ]; then
+			echo "cd"
+			comando_shell=2
+			break
+		elif [ $comando_shell -eq 0 ]; then
+			break
+		fi
+	fi	
+
+	done 
+
+	 if [ $comando_shell -eq 1 ]; then
+	 	/usr/bin/time -f "$comando %E, %U, %S" -ao "$home/.log_bash" echo $comando | bash
+	 elif [ $comando_shell -eq 2 ]; then
+	 	$comando
+	 elif [ $comando_shell -eq 0 ];then
+	 	echo $comando_shell | bash
+	 fi
 fi
- 
-echo "Meu terminal" 
-done 
 
-cd /home/fulando grep
+done 
